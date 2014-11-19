6a633c092bbb0147056628e7a1a9d380006145bb
ReneHollander <rene.hollander@hotmail.de>
2014-11-19 09:15:25 +0100

    initial commit

---
+++ src/main/java/at/hollanderpoecher/chat/Chat.java
@@ -0,0 +1,9 @@
+package at.hollanderpoecher.chat;
+
+public class Chat {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/at/hollanderpoecher/chat/test/Chat.java
@@ -0,0 +1,14 @@
+package at.hollanderpoecher.chat.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Chat {
+
+	@Test
+	public void test() {
+		fail("Not implemented");
+	}
+
+}
